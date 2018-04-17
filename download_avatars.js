4b58b3f9eff44e375ae9d83fdac3d6918abec37a
Dahlia75 <d.aljabari75@gmail.com>
2018-04-17 07:27:51 +0000

    update the program to give erorr if the arguments are not provided

--- download_avatars.js
+++ download_avatars.js
@@ -1,44 +1,46 @@
 var request = require('request');
 var fs = require('fs');
 var args = process.argv.slice(2);
+if (args.length < 2){
+  console.log('Please enter repository owner and repository name')
+} else {
+  console.log('Welcome to the GitHub Avatar Downloader!');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request'
-    }
-  };
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+  }
 
-  request(options, function(err, res, body) {
-    cb(err, body);
+  getRepoContributors(args[0], args[1], function(err, result) {
+    console.log("Errors:", err);
+    var repos = JSON.parse(result);
+    var filepath;
+    repos.forEach(function(repo) {
+      if (!fs.existsSync('avatars')){
+        fs.mkdirSync('avatars');
+      }
+      filepath = 'avatars' + '/' + repo.login + '.jpg';
+      downloadImageByURL(repo.avatar_url, filepath);
+    });
   });
+
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+         .on('error', function (err) {
+            throw err;
+         })
+         .on('response', function (response) {
+            console.log('Downloading image...');
+          })
+         .pipe(fs.createWriteStream(filePath));
+  }
 }
-
-getRepoContributors(args[0], args[1], function(err, result) {
-  console.log("Errors:", err);
-  var repos = JSON.parse(result);
-  var filepath;
-  repos.forEach(function(repo) {
-    if (!fs.existsSync('avatars')){
-      fs.mkdirSync('avatars');
-    }
-    filepath = 'avatars' + '/' + repo.login + '.jpg';
-    downloadImageByURL(repo.avatar_url, filepath);
-  });
-});
-
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-       .on('error', function (err) {
-          throw err;
-       })
-       .on('response', function (response) {
-          console.log('Downloading image...');
-        })
-       .pipe(fs.createWriteStream(filePath));
-}
-
 //downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
