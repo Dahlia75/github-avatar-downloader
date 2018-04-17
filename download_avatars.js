7ab6946e0d81ea2bacc6f0460044127725049269
Dahlia75 <d.aljabari75@gmail.com>
2018-04-17 03:06:57 +0000

    print the urls for avatars

--- download_avatars.js
+++ download_avatars.js
@@ -19,9 +19,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  var repos = JSON.parse(result);
+  repos.forEach(function(repo) {
+    console.log(repo.avatar_url);
+  });
 });
-
-
-
-
